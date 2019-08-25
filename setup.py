b18f12b9e417c65be006e064347a1f9520c55bc4
Max Voss <b.voss@campus.tu-berlin.de>
2019-08-25 19:56:41 +0200

    more savings

---
+++ setup.py
@@ -0,0 +1,21 @@
+import os
+import setuptools
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setuptools.setup(
+    name = "Sxml",
+    version = "0.1",
+    author = "Brumo Maximilian Voss",
+    author_email = "bruno.m.voss@gmail.com",
+    description = ("dump and load simple python data types to xml"),
+    packages=setuptools.find_packages('src'),
+    package_dir={'': 'src'},
+    license = "MIT",
+)
