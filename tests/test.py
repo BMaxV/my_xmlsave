bd20802ed4333cc342fbfc48c97a6bc26f563ac5
max <bruno.m.voss@gmail.com>
2024-06-16 10:13:21 +0200

    tests for benchmarking vs. pickle

--- tests/test.py
+++ tests/test.py
@@ -2,7 +2,7 @@ from my_save import sxml_main
 import unittest
 
 
-class TestMySave:
+class TestMySave(unittest.TestCase):
     def test4(self):
         l = [[(1, 2)], [(5, 6), (7, 8)], [(9, 10), (11, 12)]]
         d = {"eh": l}
@@ -47,7 +47,64 @@ class TestMySave:
         new_d = sxml_main.read("int_dict_key.xml")
 
         assert d == new_d
+    def test_dict_int_keys(self):
+        d = {"questtasks":[
+                ("WalkTask", ((0,-2,0),) , ),          # this can coincide with the location I purposefully place some stuff at.
+                ("GetTask", ("Axe",)     , ), 
+                ("WalkTask", ((-3,3,0),) ,  ), 
+                ("HarvestTask",("Wood",) , ),
+                ("WalkTask", ((0,-3,0),) ,  ), 
+                ("GetTask", ("Tent",),      ),             
+                ("PlaceBlueprintTask",("Tent",),    {"vuid":True} ),   # set a thing for a virtual uid, create one from the task or plan step or whatever, and save it.
+                ("PlaceBlueprintTask",("campfire",),{"vuid":True} ),# then reference it later.
+                ("BuildTask",("Tent",),{"vuid":True} ),
+                ("BuildTask",("campfire",),{"vuid":True} ),  # "1":"hello"}
+            ]}
+        sxml_main.write("quests.xml", d)
+        new_d = sxml_main.read("quests.xml")
+
+        assert d == new_d
+
+
+def test_benchmark():
+    import time
+    
+    t0=time.time()
+    d=sxml_main.read("my_test_file.xml")
+    sxml_main.write("test_bench_big.xml", d)
+    t1=time.time()
+    print("me",t1-t0)
+
+def test_pickle():
+    import time
+    import pickle
+    
+    d=sxml_main.read("my_test_file.xml")
+    t0=time.time()
+    with open("pickletest.db","ab") as f:
+        pickle.dump(d,f)
+    with open("pickletest.db","rb") as f:
+        my_data = pickle.load(f)
+    t1=time.time()
+    print("pickle test",t1-t0)
+    assert my_data == d
+
+def test_pandas1():
+    import time
+    import pandas as pd
+
+    d=sxml_main.read("my_test_file.xml")
+    t0=time.time()
+    df = pd.DataFrame.from_dict(d)
+    df.to_csv("this_test.csv")
+    my_data = pd.read_csv("this_test.csv")
+    my_data = my_data.to_dict()
+    t1=time.time()
+    assert my_data == d
+    print("pandas csv",t1-t0)
 
 
 if __name__ == "__main__":
     unittest.main()
+    test_pickle()
+    test_benchmark()
